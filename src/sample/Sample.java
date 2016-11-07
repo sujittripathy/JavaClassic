7285b071603f524451af817e8e59918812165ee3
stripathy <sujit.tripathy@icloud.com>
2016-11-07 13:55:30 -0800

    ok

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -1,7 +1,9 @@
 package sample;
 
 import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -23,5 +25,18 @@ public class Sample {
        builder.insert(2,"Hello");
         System.out.println("String val > "+builder.toString());
 
+        LinkedHashMap<String,Integer> lhm= new LinkedHashMap<>();
+        lhm.put("hello",10);
+        lhm.put("world",2);
+        lhm.put("apple",99);
+
+        lhm.forEach((a,b) -> System.out.println(a));
+        System.out.println("--------------------");
+        TreeMap<String,Integer> tm= new TreeMap<>();
+        tm.put("hello",10);
+        tm.put("world",2);
+        tm.put("apple",99);
+
+        tm.forEach((a,b) -> System.out.println(a));
     }
 }
