fec3faba27c86aa205e7fe0b5d2ebb7b3085adb5
stripathy <sujit.tripathy@icloud.com>
2017-01-05 10:25:10 -0800

    added .gitignore file and updated ctci module file

--- src/ctci/UniqueStringCharacter.java
+++ src/ctci/UniqueStringCharacter.java
@@ -1,5 +1,23 @@
-class UniqueStringCharacter{
-  public static void main(String str[]){
-    
+package ctci;
+
+public class UniqueStringCharacter{
+  public static void main(String s[]){
+    String str = "UniqueStringCharacter";
+    char charArray[] = str.toCharArray();
+    boolean match = false;
+    for(int i=0;i<charArray.length;i++){
+      char c=charArray[i];
+      for(int j=i+1;j<charArray.length;j++){
+        if(c == charArray[j]){
+          System.out.println("match : "+c +", index" + j);
+          match = true;
+          break;
+        }
+      }
+      if(match){
+        System.out.println("String character is not unique : "+str);
+        break;
+      }
+    }
   }
 }
